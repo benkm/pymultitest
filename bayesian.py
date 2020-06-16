ae375592249d2c7abbaa5c3d6f0b71c65a0d5af7
Ben Kitching-Morley <bkm1n18@soton.ac.uk>
2020-06-16 15:19:21 +0100

    Fixed a couple bugs with 6 param fit

--- bayesian.py
+++ bayesian.py
@@ -15,11 +15,12 @@ GL_min = 12.8
 GL_max = 76.8
 no_samples = 500
 
+
 # Use the same cut as Andreas for now
 g_s_cut, Bbar_s_cut, N_s_cut, L_s_cut, samples_cut, m_s_cut = cut(GL_min, GL_max, g_s, Bbar_s, N_s, L_s, samples, m_s)
 
 
-cov_matrix = cov_matrix_calc(samples_cut, m_s_cut)
+cov_matrix, different_ensemble = cov_matrix_calc(samples_cut, m_s_cut)
 cov_1_2 = numpy.linalg.cholesky(cov_matrix)
 cov_inv = numpy.linalg.inv(cov_1_2)
 
@@ -33,7 +34,7 @@ params2 = numpy.array(res2.x)
 
 
 # Describes how broad a prior is used
-prior_size = 100
+prior_size = 10
 
 
 # Use uniform priors
@@ -43,6 +44,31 @@ def prior(cube, ndim, nparams):
     mini = min(params1[i], params2[i])
     Delta = (maxi - mini)
     cube[i] = cube[i] * prior_size * Delta + mini - Delta
+
+
+def prior_7_params(cube, ndim, nparams):
+  nu_min, nu_max = 0.5, 0.9
+  f0_min, f0_max = -20, 20
+  lambduh_min, lambduh_max = -10, 10
+  omega_min = - 1 / nu_max
+  #               alpha, c, f0, f1, lambduh, nu, omega
+  parameter_mins = [-1, -0.9, f0_min, 3, -10, nu_min, -0.01]
+  parameter_maxs = [1, -0.5, f0_max, 8, 10, nu_max, 0.01]
+
+  for i in range(len(parameter_mins)):
+    cube[i] = cube[i] * (parameter_maxs[i] - parameter_mins[i]) + parameter_mins[i]
+
+
+def prior_6_params(cube, ndim, nparams):
+  nu_min, nu_max = 0.5, 0.9
+  f0_min, f0_max = -20, 20
+  lambduh_min, lambduh_max = -10, 10
+  #                alpha, c,   f0,  f1, lambduh, nu
+  parameter_mins = [-10, -1, f0_min, -10, -10, nu_min]
+  parameter_maxs = [10, 1, f0_max, 10, 10, nu_max]
+
+  for i in range(len(parameter_mins)):
+    cube[i] = cube[i] * (parameter_maxs[i] - parameter_mins[i]) + parameter_mins[i]
 
 
 def loglike1(cube, ndim, nparams):
@@ -58,9 +84,45 @@ def loglike1(cube, ndim, nparams):
 
 def loglike2(cube, ndim, nparams):
   alpha, c, f0, f1, lambduh, nu, omega = cube[0], cube[1], cube[2], cube[3], cube[4], cube[5], cube[6]
+
   x = alpha, c, f0, f1, lambduh, nu, omega
 
   chisq = chisq_calc(x, cov_inv, model2)
+
+  loglikelihood = numpy.sum(-0.5 * chisq)
+
+  return loglikelihood
+
+
+def loglike3(cube, ndim, nparams):
+  alpha, c, f0, f1, lambduh, nu = cube[0], cube[1], cube[2], cube[3], cube[4], cube[5]
+  x = alpha, c, f0, f1, lambduh, nu
+
+  chisq = chisq_calc(x, cov_inv, model3)
+
+  loglikelihood = numpy.sum(-0.5 * chisq)
+
+  return loglikelihood
+
+
+def loglike1_small(cube, ndim, nparams):
+  alpha, f0, f1, lambduh, nu = cube[0], cube[2], cube[3], cube[4], cube[5]
+
+  x = alpha, f0, f1, lambduh, nu
+
+  chisq = chisq_calc(x, cov_inv, model1_small)
+
+  loglikelihood = numpy.sum(-0.5 * chisq)
+
+  return loglikelihood
+
+
+def loglike2_small(cube, ndim, nparams):
+  alpha, c, f0, f1, lambduh, nu, omega = cube[0], cube[1], cube[2], cube[3], cube[4], cube[5], cube[6]
+
+  x = alpha, f0, f1, lambduh, nu
+
+  chisq = chisq_calc(x, cov_inv, model2_small)
 
   loglikelihood = numpy.sum(-0.5 * chisq)
 
@@ -72,20 +134,12 @@ parameters = ["alpha", "c", "a", "b", "lambduh", "nu", "omega"]
 n_params = len(parameters)
 
 
-def run_pymultinest(likelihood_function, model, label):
+def run_pymultinest(likelihood_function, model, label, prior):
   # run MultiNest
   pymultinest.run(likelihood_function, prior, n_params, outputfiles_basename=datafile + label, resume=False)
   json.dump(parameters, open(datafile + label + 'params.json', 'w')) # save parameter names
 
-  # plot the distribution of a posteriori possible models
-  plt.figure()
-  plt.plot(g_s_cut * L_s_cut, m_s_cut / g_s_cut, '+ ', color='red', label='data')
   run = pymultinest.Analyzer(outputfiles_basename=datafile + label, n_params=n_params)
-  for (alpha, c, f0, f1, lambduh, nu, omega) in run.get_equal_weighted_posterior()[::100,:-1]:
-    plt.plot(g_s_cut * L_s_cut, model(N_s_cut, g_s_cut, L_s_cut, Bbar_s_cut, alpha, c, f0, f1, lambduh, nu, omega) / g_s_cut, '-', color='blue', alpha=0.3, label='data')
-
-  plt.savefig(datafile + 'posterior' + label + '.pdf')
-  plt.close()
 
   a_lnZ = run.get_stats()['global evidence']
   print()
@@ -97,14 +151,29 @@ def run_pymultinest(likelihood_function, model, label):
   return a_lnZ
 
 
-model1_lnZ = run_pymultinest(loglike1, model1, '1')
-model2_lnZ = run_pymultinest(loglike2, model2, '2')
+# model1_lnZ = run_pymultinest(loglike1, model1, '1', prior_7_params)
+# model2_lnZ = run_pymultinest(loglike2, model2, '2', prior_7_params)
+model3_lnZ = run_pymultinest(loglike3, model3, '3', prior_6_params)
 
-E = numpy.exp(model1_lnZ - model2_lnZ)
+# E = numpy.exp(model1_lnZ - model2_lnZ)
 
 # Get posterior distributions
-analysis1 = pymultinest.analyse.Analyzer(n_params, outputfiles_basename=datafile + '1')
-numpy.save(f"{datafile}posterior_data1_size{prior_size}.npy", analysis1.get_equal_weighted_posterior())
+# analysis1 = pymultinest.analyse.Analyzer(n_params, outputfiles_basename=datafile + '1')
+# numpy.save(f"{datafile}posterior_data1.npy", analysis1.get_equal_weighted_posterior())
 
-analysis2 = pymultinest.analyse.Analyzer(n_params, outputfiles_basename=datafile + '2')
-numpy.save(f"{datafile}posterior_data2_size{prior_size}.npy", analysis2.get_equal_weighted_posterior())
+# analysis2 = pymultinest.analyse.Analyzer(n_params, outputfiles_basename=datafile + '2')
+# numpy.save(f"{datafile}posterior_data2.npy", analysis2.get_equal_weighted_posterior())
+
+analysis3 = pymultinest.analyse.Analyzer(n_params, outputfiles_basename=datafile + '3')
+numpy.save(f"{datafile}posterior_data3.npy", analysis3.get_equal_weighted_posterior())
+
+
+# Run also for the smaller models without corrections to scaling
+# model1_small_lnZ = run_pymultinest(loglike1_small, model1_small, '1_small')
+# model2_small_lnZ = run_pymultinest(loglike2_small, model2_small, '2_small')
+
+# analysis1_small = pymultinest.analyse.Analyzer(n_params - 2, outputfiles_basename=datafile + '1_small')
+# numpy.save(f"{datafile}posterior_data1_small.npy", analysis1_small.get_equal_weighted_posterior())
+
+# analysis2_small = pymultinest.analyse.Analyzer(n_params - 2, outputfiles_basename=datafile + '2_small')
+# numpy.save(f"{datafile}posterior_data2_small.npy", analysis2_small.get_equal_weighted_posterior())
--- posterior_analysis.py
+++ posterior_analysis.py
@@ -4,40 +4,78 @@ import pdb
 import datetime
 import time
 today = datetime.date.fromtimestamp(time.time())
+from chisq_fit import *
+
+directory = f'posterior_data/2020_6_16/'
+data = numpy.load(f"{directory}posterior_data1.npy")
+data3 = numpy.load(f"{directory}posterior_data3.npy")
 
 
+# Extract the points with small omega
+# data2 = data[data[:, 6] > -0.01]
+# data3 = data2[data2[:, 7] > -20]
 
-# Describes how broad a prior is used
-for prior_size in [3, 10, 100]:
 
-  directory = f'posterior_data/{today.year}_{today.month}_{today.day}/'
-  data1 = numpy.load(f"{directory}posterior_data1_size{prior_size}.npy")
-  data2 = numpy.load(f"{directory}posterior_data2_size{prior_size}.npy")
-
-  fig, axes = plt.subplots(2, 4)
-
-  param_names = ['alpha', 'c', 'f0', 'f1', 'lambduh', 'nu', 'omega']
+def plot_data(data, param_names):
+  r = (len(param_names) + 1) // 2
+  fig, axes = plt.subplots(2, r)
 
   # There are 7 parameters
-  for i in range(7):
-    axes[i // 4, i % 4].scatter(data1[:, i], data1[:, 7])
-    axes[i // 4, i % 4].set_xlim(min(data1[:, i]), max(data1[:, i]))
-    axes[i // 4, i % 4].set_xlabel(param_names[i])
-    axes[i // 4, i % 4].set_ylabel('log(L)')
+  for i in range(len(param_names)):
+    axes[i // r, i % r].scatter(data[:, i], data[:, 7])
+    axes[i // r, i % r].set_xlim(min(data[:, i]), max(data[:, i]))
+    axes[i // r, i % r].set_xlim(min(data[:, i]), max(data[:, i]))
+    axes[i // r, i % r].set_xlabel(param_names[i])
+    axes[i // r, i % r].set_ylabel('log(L)')
 
-  plt.savefig(f"{directory}posterior_plot_size{prior_size}")
+  plt.savefig(f"{directory}posterior_plot1.png")
   plt.show()
   plt.close()
 
-  fig, axes = plt.subplots(2, 4)
 
-  # There are 7 parameters
-  for i in range(7):
-    axes[i // 4, i % 4].scatter(data2[:, i], data2[:, 7])
-    axes[i // 4, i % 4].set_xlim(min(data2[:, i]), max(data2[:, i]))
-    axes[i // 4, i % 4].set_xlabel(param_names[i])
-    axes[i // 4, i % 4].set_ylabel('log(L)')
+plot_data(data, ['alpha', 'c', 'f0', 'f1', 'lambduh', 'nu', 'omega'])
+plot_data(data3, ['alpha', 'c', 'f0', 'f1', 'lambduh', 'nu'])
 
+# # Describes how broad a prior is used
+# for model in ['1', '2']:
+#   for prior_size in [3, 10, 100]:
+#     directory = f'posterior_data/{today.year}_{today.month}_{today.day}/'
+
+#     data = numpy.load(f"{directory}posterior_data{model}_size{prior_size}.npy")
+
+#     fig, axes = plt.subplots(2, 4)
     
-  plt.savefig(f"{directory}posterior_plot_size{prior_size}")
-  plt.show()
+#     param_names = ['alpha', 'c', 'f0', 'f1', 'lambduh', 'nu', 'omega']
+
+#     # There are 7 parameters
+#     for i in range(7):
+#       axes[i // 4, i % 4].scatter(data[:, i], data[:, 7])
+#       axes[i // 4, i % 4].set_xlim(min(data[:, i]), max(data[:, i]))
+#       axes[i // 4, i % 4].set_xlabel(param_names[i])
+#       axes[i // 4, i % 4].set_ylabel('log(L)')
+
+#     plt.savefig(f"{directory}posterior_plot{model}_size{prior_size}")
+#     plt.show()
+#     plt.close()
+
+
+# # Describes how broad a prior is used
+# for model in ['1_small', '2_small']:
+#   for prior_size in [10]:
+#     directory = f'posterior_data/{today.year}_{today.month}_{today.day}/'
+#     data = numpy.load(f"{directory}posterior_data{model}_size{prior_size}.npy")
+
+#     fig, axes = plt.subplots(2, 3)
+    
+#     param_names = ['alpha', 'f0', 'f1', 'lambduh', 'nu']
+
+#     # There are 7 parameters
+#     for i in range(5):
+#       axes[i // 3, i % 3].scatter(data[:, i], data[:, 7])
+#       axes[i // 3, i % 3].set_xlim(min(data[:, i]), max(data[:, i]))
+#       axes[i // 3, i % 3].set_xlabel(param_names[i])
+#       axes[i // 3, i % 3].set_ylabel('log(L)')
+
+#     plt.savefig(f"{directory}posterior_plot{model}_size{prior_size}")
+#     plt.show()
+#     plt.close()
