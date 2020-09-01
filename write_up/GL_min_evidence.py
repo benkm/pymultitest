cf1d9ee1f86938d598a55e4ca56aa1af0fd8390e
Ben Kitching-Morley <bkm1n18@soton.ac.uk>
2020-09-01 13:29:26 +0100

    Produced early version of dependence of evidence and p-value on final result graph

--- write_up/GL_min_evidence.py
+++ write_up/GL_min_evidence.py
@@ -3,27 +3,35 @@ import pickle
 from chisq_fit3 import *
 from tqdm import tqdm
 
-directory = f"posterior_data/{today.year}_{today.month}_{today.day}/"
-points = 200
-no_samples = 10
+# directory = f"posterior_data/{today.year}_{today.month}_{today.day}/"
+directory = f"posterior_data/{today.year}_{8}_{31}/"
 
-GL_min_graph = 10
-GL_mins = numpy.sort(list(set(numpy.around(g_s * L_s, 1))))
+points = 1000
+no_samples = 20
+model1 = model27
+model2 = model28
 
-# Because the model has 5 parameters we need at least 6 data points
-GL_maxi = numpy.around(numpy.sort(g_s * L_s)[-6], 1)
-GL_max = 76.8
+GL_min_graph = 7
+GL_mins = pickle.load(open(f"{directory}evidence_GL_mins_points{points}_samples{no_samples}.pcl", "rb"))
 
-results = pickle.load(open(f"{directory}evidence_results_points{points}_samples{no_samples}.pcl", "rb"))
+# results1 = pickle.load(open(f"{directory}evidence_results1_points{points}_samples{no_samples}.pcl", "rb"))
+# results2 = pickle.load(open(f"{directory}evidence_results2_points{points}_samples{no_samples}.pcl", "rb"))
+
+results1 = pickle.load(open(f"{directory}evidence_model27_points{points}_samples{no_samples}.pcl", "rb"))
+results2 = pickle.load(open(f"{directory}evidence_model28_points{points}_samples{no_samples}.pcl", "rb"))
+results = results1 - results2
+
 results = results[GL_mins >= GL_min_graph]
 GL_mins = GL_mins[GL_mins >= GL_min_graph]
 
-results = results[GL_mins <= GL_maxi]
-GL_mins = GL_mins[GL_mins <= GL_maxi]
-
-plt.errorbar(GL_mins, numpy.mean(results, axis=1), yerr=numpy.std(results, axis=1), capsize=5, color='k', label='log(Evidence)')
+plt.errorbar(GL_mins, numpy.mean(results, axis=1), yerr=numpy.std(results, axis=1), capsize=5, color='k', label='log(Evidence)', ls='')
 ax = plt.gca()
 ax.set_ylabel("log(Evidence)")
+
+# Show the Jeffrey's scale 
+plt.fill_between([min(GL_mins) / 2, max(GL_mins) * 2], [0, 0], [1, 1], color='g', alpha=0.2, label='Insignificant 0 < x < 1')
+plt.fill_between([min(GL_mins) / 2, max(GL_mins) * 2], [1, 1], [2.5, 2.5], color='r', alpha=0.2, label='Significant 1 < x < 2.5')
+plt.fill_between([min(GL_mins) / 2, max(GL_mins) * 2], [2.5, 2.5], [max(numpy.mean(results, axis=1)) * 1.1, max(numpy.mean(results, axis=1)) * 1.1], color='b', alpha=0.2, label='Very Significant, x > 2.5')
 
 # Add in the frequentist side of things
 pvalues_1 = []
@@ -37,38 +45,53 @@ for GL_min in tqdm(GL_mins):
   cov_inv = numpy.linalg.inv(cov_1_2)
 
   alpha_range = [-0.1, 0.1]
-  f0_range = [-10, 10]
-  f1_range = [-10, 10]
-  lambduh_range = [0, 2]
-  nu_range = [0.5, 0.9]
+  c_range = [-2, 2]
+  f0_range = [-4, 4]
+  f1_range = [-2, 2]
+  lambduh_range = [-1, 3]
+  nu_range = [0.3, 1.1]
+  omega_range = [0, 2]
 
-  bounds = ([alpha_range[0], f0_range[0], f1_range[0], lambduh_range[0], nu_range[0]],
+  if model1 is model50 and model2 is model51:
+    bounds = ([alpha_range[0], f0_range[0], f1_range[0], lambduh_range[0], nu_range[0]],
     [alpha_range[1], f0_range[1], f1_range[1], lambduh_range[1], nu_range[1]])
+    x0 = [0, 0.657, -0.038, 1, 2 / 3]
+  
+  if model1 is model27 and model2 is model28:
+    bounds = ([alpha_range[0], c_range[0], f0_range[0], f1_range[0], lambduh_range[0], nu_range[0], omega_range[0]],
+    [alpha_range[1], c_range[1], f0_range[1], f1_range[1], lambduh_range[1], nu_range[1], omega_range[1]])
+    # x0 = [0, 0, 0.657, -0.038, 1, 2 / 3, 0.8]
+    x0 = [-0.01612187, -0.87133898,  4.        , -0.44064317,  0.97991928, 0.7127984 ,  0.01029297]
 
-  x0 = [0, 0.657, -0.038, 1, 2 / 3]
 
   kwargs = {"m_s": m_s_cut, "N_s": N_s_cut, "g_s": g_s_cut, "L_s": L_s_cut, "Bbar_s": Bbar_s_cut}
 
-  res50 = least_squares(res_function, x0, bounds=bounds, args=(cov_inv, model50),
+  res1 = least_squares(res_function, x0, bounds=bounds, args=(cov_inv, model1),
   kwargs=kwargs, method='dogbox')
-  chisq50 = chisq_calc(res50.x, cov_inv, model50, **kwargs)
-  dof = g_s_cut.shape[0] - len(res50.x)
-  pvalues_1.append(chisq_pvalue(dof, chisq50))
+  chisq1 = chisq_calc(res1.x, cov_inv, model1, **kwargs)
+  dof = g_s_cut.shape[0] - len(res1.x)
+  pvalues_1.append(chisq_pvalue(dof, chisq1))
 
-  res51 = least_squares(res_function, x0, bounds=bounds, args=(cov_inv, model51),
+  res2 = least_squares(res_function, x0, bounds=bounds, args=(cov_inv, model2),
   kwargs=kwargs, method='dogbox')
-  chisq51 = chisq_calc(res51.x, cov_inv, model51, **kwargs)
-  dof = g_s_cut.shape[0] - len(res51.x)
-  pvalues_2.append(chisq_pvalue(dof, chisq51))
+  chisq2 = chisq_calc(res2.x, cov_inv, model2, **kwargs)
+  dof = g_s_cut.shape[0] - len(res2.x)
+  pvalues_2.append(chisq_pvalue(dof, chisq2))
 
 
 ax = plt.gca()
 fig = plt.gcf()
+ax.set_xlabel("GL_min")
+ax.set_xlim(GL_min_graph, 40)
 
 ax2 = ax.twinx()
 ax2.set_ylabel('p-value')
-ax2.plot(GL_mins, pvalues_1, marker='s', markerfacecolor='none', label="model1")
-ax2.plot(GL_mins, pvalues_2, marker='s', markerfacecolor='none', label="model2")
+ax2.plot(GL_mins, pvalues_1, marker='s', markerfacecolor='none', label="model1", ls='')
+ax2.plot(GL_mins, pvalues_2, marker='s', markerfacecolor='none', label="model2", ls='')
+
+# Show our acceptance threshold (alpha)
+ax2.plot(GL_mins, [0.05, ] * len(GL_mins), ls='--', label='alpha = 0.05', color='r')
+
 
 fig.tight_layout()
 
@@ -77,5 +100,11 @@ ax2.tick_params(direction='in')
 ax.legend(loc='upper left')
 ax2.legend(loc='upper right')
 
+# Align 0 for both axes
+left1, right1 = ax.get_ylim()
+left2, right2 = ax2.get_ylim()
+
+ax.set_ylim(0, right1)
+ax2.set_ylim(0, right2)
 
 plt.show()
--- write_up/bayesian_3.py
+++ write_up/bayesian_3.py
@@ -3,36 +3,54 @@
 from bayesian_2 import *
 from tqdm import tqdm
 
+# PARAMETERS
+model1 = model27
+model2 = model28
+points = 2000
+no_samples = 30
+
 if __name__ == "__main__":
   # Where the results are saved
   directory = f'model_output_{today.year}_{today.month}_{today.day}/'
 
-  alpha_range = [-0.2, 0.2]
+  alpha_range = [-0.1, 0.1]
+  c_range = [-2, 2]
   f0_range = [-4, 4]
   f1_range = [-2, 2]
-  lambduh_range = [-1, 3]
-  nu_range = [0.3, 1.1]
+  lambduh_range = [0, 2]
+  nu_range = [0.5, 0.9]
+  omega_range = [0, 2]
 
-  prior_range = [alpha_range, f0_range, f1_range, lambduh_range, nu_range]
+  if model1 is model50 and model2 is model51:
+    prior_range = [alpha_range, f0_range, f1_range, lambduh_range, nu_range]
+  
+  if model1 is model27 and model2 is model28:
+    prior_range = [alpha_range, c_range, f0_range, f1_range, lambduh_range, nu_range, omega_range]
+
   n_params = len(prior_range)
 
-  # Because the model has 5 parameters we need at least 6 data points
-  GL_maxi = numpy.around(numpy.sort(g_s * L_s)[-6], 1)
+  # Because the model has n parameters we need at least (n+1) data points
+  GL_maxi = numpy.around(numpy.sort(g_s * L_s)[-(n_params + 1)], 1)
   GL_max = 76.8
 
-  points = 300
-  no_samples = 15
   GL_mins = numpy.sort(list(set(numpy.around(g_s * L_s, 1))))
   GL_mins = GL_mins[GL_mins <= GL_maxi]
 
-  results = numpy.zeros((len(GL_mins), no_samples))
+  # Apply a lower-bound GL_min, as we aren't particularly interested in results
+  # below a certain threshold
+  GL_mini = 5
+  GL_mins = GL_mins[GL_mins >= GL_mini]
+
+  results1 = numpy.zeros((len(GL_mins), no_samples))
+  results2 = numpy.zeros((len(GL_mins), no_samples))
 
   def f0(GL_min):
-    results_piece = numpy.zeros(no_samples)
+    results_piece1 = numpy.zeros(no_samples)
+    results_piece2 = numpy.zeros(no_samples)
 
     for i in tqdm(range(no_samples)):
-      analysis1 = run_pymultinest(prior_range, model50, GL_min, GL_max, n_params, directory, n_live_points=points, sampling_efficiency=0.3, clean_files=True, return_analysis_small=True)
-      analysis2 = run_pymultinest(prior_range, model51, GL_min, GL_max, n_params, directory, n_live_points=points, sampling_efficiency=0.3, clean_files=True, return_analysis_small=True)
+      analysis1 = run_pymultinest(prior_range, model1, GL_min, GL_max, n_params, directory, n_live_points=points, sampling_efficiency=0.3, clean_files=False, return_analysis_small=True)
+      analysis2 = run_pymultinest(prior_range, model2, GL_min, GL_max, n_params, directory, n_live_points=points, sampling_efficiency=0.3, clean_files=False, return_analysis_small=True)
 
       # stats1 = analysis1.get_stats()
       # stats2 = analysis2.get_stats()
@@ -40,20 +58,23 @@ if __name__ == "__main__":
       # E1 = stats1['global evidence']
       # E2 = stats2['global evidence']
 
-      E1 = analysis1[0]
-      E2 = analysis2[0]
+      results_piece1[i] = analysis1[0]
+      results_piece2[i] = analysis2[0]
 
-      results_piece[i] = E1 - E2
+    return numpy.array([results_piece1, results_piece2])
 
-    return results_piece
+  # for j, GL_min in enumerate(GL_mins):
+  #   results = f0(GL_min)
+  #   results1[j] = results[0]
+  #   results2[j] = results[1]
 
-  for j, GL_min in enumerate(GL_mins):
-    results[j] = f0(GL_min)
+  p = Pool(4)
+  results = numpy.array(p.map(f0, GL_mins, chunksize=1))
+  p.close()
+  
+  results1 = results[:, 0, :]
+  results2 = results[:, 1, :]
 
-  # p = Pool(4)
-  # results = p.map(f0, GL_mins, chunksize=1)
-  # p.close()
-
-  pickle.dump(results, open(f"{directory}evidence_results_points{points}_samples{no_samples}.pcl", "wb"))
+  pickle.dump(results1, open(f"{directory}evidence_{model1.__name__}_points{points}_samples{no_samples}.pcl", "wb"))
+  pickle.dump(results2, open(f"{directory}evidence_{model2.__name__}_points{points}_samples{no_samples}.pcl", "wb"))
   pickle.dump(GL_mins, open(f"{directory}evidence_GL_mins_points{points}_samples{no_samples}.pcl", "wb"))
-
